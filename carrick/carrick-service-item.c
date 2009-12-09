f40046c72ff9ba7b26f0ec9576d5e07800f11b03
Ross Burton <ross@linux.intel.com>
2009-12-09 13:19:27 +0000

    Use carrick_shell_hide

--- carrick/carrick-service-item.c
+++ carrick/carrick-service-item.c
@@ -52,12 +52,6 @@ G_DEFINE_TYPE (CarrickServiceItem, carrick_service_item, GTK_TYPE_EVENT_BOX)
 #define INVALID_CONNMAN _("Critical Error: Unable to access Connection Manager")
 #define MISSING_3G_HW _("ERROR: No 3G Hardware detected")
 
-/*
- * TODO: A last-minute pre-release fix for MB#7304; better solution
- *       to be implement.
- */
-extern MplPanelClient *panel_client;
-
 enum {
   PROP_0,
   PROP_DRAGGABLE,
@@ -643,9 +637,8 @@ _connect_button_cb (GtkButton          *connect_button,
 						      connect_notify_cb,
 						      item);
 
-            if (panel_client)
-              mpl_panel_client_request_hide (panel_client);
-	  }
+            carrick_shell_hide ();
+ 	  }
         }
     }
 }
